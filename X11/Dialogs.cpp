da0f81ae2c7fcf094c534e2338d9d3d2364cdd4c
wavesoft <johnys2@gmail.com>
2013-04-23 17:28:35 +0200

    Linux tune-ups

--- X11/Dialogs.cpp
+++ X11/Dialogs.cpp
@@ -4,13 +4,12 @@
 #include "../Dialogs.h"
 
 typedef struct {
-    int type;
     int result;
     const char * text;
 } DialogData;
 
 static gboolean display_dialog( gpointer user_data ) {
-    DialogData *dialog_data = user_data;
+    DialogData *dialog_data = (DialogData*) user_data;
     GtkWidget *dialog;
     
     /* Fetch window */
@@ -21,21 +20,22 @@ static gboolean display_dialog( gpointer user_data ) {
         GTK_WINDOW(window), 
         GTK_DIALOG_DESTROY_WITH_PARENT, 
         GTK_MESSAGE_QUESTION, 
-        GTK_BUTTONS_YES_NO, ( const gchar * ) user_data->text);
+        GTK_BUTTONS_YES_NO, 
+        dialog_data->text);
 
     /* Set caption */
     gtk_window_set_title(GTK_WINDOW(dialog), "CernVM Web API");
     dialog_data->result = gtk_dialog_run(GTK_DIALOG(dialog));
     gtk_main_quit();  // Quits the main loop run in MessageBox()
+    gtk_widget_destroy(dialog);
 
     return FALSE;
 }
 
 bool CVMConfirmDialog(const FB::BrowserHostPtr& host, FB::PluginWindow* win, std::string message) {
     DialogData dialog_data;
-    dialog_data.type = type;
     dialog_data.text = (const char * ) message.c_str();
-    gtk_idle_add(display_dialog, &dialog_data);
+    g_idle_add(display_dialog, &dialog_data);
     gtk_main();
-    return (dialog_data.result == GTK_RESPONSE_OK);
+    return (dialog_data.result == GTK_RESPONSE_YES);
 }
