31bb5b3031403f054b5e4284f26e409ff300cf2a
Ioannis Charalampidis <wavesoft@github.com>
2013-08-12 18:25:11 +0200

    Update install locations on the js library

--- js/src/core.js
+++ js/src/core.js
@@ -307,7 +307,7 @@ _NS_.startCVMWebAPI = function( cbOK, cbFail, setupEnvironment ) {
                             if (BrowserDetect.browser == "Firefox") {
                                 
                                 // For firefox, just point to the XPI, the user will be prompted
-                                window.location = "http://labs.wavesoft.gr/micro/res/cvmwebapi-1.2.4.xpi";
+                                window.location = "http://cernvm.cern.ch/releases/webapi/plugin/cvmwebapi-1.2.6.xpi";
                                 
                             } else if (BrowserDetect.browser == "Chrome") {
                                 
